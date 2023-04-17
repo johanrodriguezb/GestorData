14b48754660bd40bd2edc0151c5b86afd77e8330
johan rodriguez <johanrodriguezbermudez@gmail.com>
2023-04-16 19:18:35 -0500

    se quita base de datos

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ class Conectar
     public static function conexion()
     {
 
-        $conexion = new mysqli("localhost", "root", "", "gestion_datos");
+        $conexion = new mysqli("us-cdbr-east-06.cleardb.net", "b4a4ad00f86c45", "9126dce2da3a981", "heroku_44f0cc8b5ad1524");
         return $conexion;
     }
 }
