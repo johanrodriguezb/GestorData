352a74cd7b617f8751822485a65b76ef4ba8acd0
johanrodriguezb <stevenb.09@hotmail.com>
2024-04-16 19:48:45 -0500

    Gestor de datos

--- models/AlumnosModel.php
+++ models/AlumnosModel.php
@@ -1,5 +1,5 @@
 <?php
-    
+
 class Alumnos_model
 {
     private $db;
@@ -20,7 +20,7 @@ class Alumnos_model
     {
         session_start();
         $nombre = $_SESSION["nombreD"];
-        $sql = "SELECT * FROM usuarios a inner join tipoDocumento b on a.tipoDocumento = b.id_documento inner join rol c on a.idRol = c.id_rol inner join Cursos d on a.Curso = d.id_curso left join instructorescursos e on a.id_usuario = e.id_alumno WHERE a.idRol = 3 and e.nombre_instructor = '$nombre' ORDER BY e.id_alumno ASC";
+        $sql = "SELECT * FROM usuarios a inner join tipodocumento b on a.tipoDocumento = b.id_documento inner join rol c on a.idRol = c.id_rol inner join cursos d on a.Curso = d.id_curso left join instructorescursos e on a.id_usuario = e.id_alumno WHERE a.idRol = 3 and e.nombre_instructor = '$nombre' ORDER BY e.id_alumno ASC";
         $resultado = $this->db->query($sql);
         while ($row = $resultado->fetch_assoc()) {
             $this->alumnos[] = $row;
@@ -28,26 +28,27 @@ class Alumnos_model
         return $this->alumnos;
     }
 
-    public function agregarObser($id,$observacion){
+    public function agregarObser($id, $observacion)
+    {
         $sql = $this->db->query("UPDATE instructorescursos SET Observacion = '$observacion' WHERE id_alumno = $id");
         $alerta = 'alert-success';
         $mensaje = 'Observación Agregada';
         $this->redireccionalumnos($alerta, $mensaje);
     }
-    
-    public function aceptarAlumno($id){
+
+    public function aceptarAlumno($id)
+    {
         $sql = $this->db->query("UPDATE instructorescursos SET EstadoAceptado = 1 WHERE id_alumno = $id");
         $alerta = 'alert-success';
         $mensaje = 'Alumno Aceptado';
         $this->redireccionalumnos($alerta, $mensaje);
     }
 
-    public function eliminarAlumno($id){
+    public function eliminarAlumno($id)
+    {
         $sql = $this->db->query("UPDATE instructorescursos SET EstadoAceptado = 0 WHERE id_alumno = $id");
         $alerta = 'alert-danger';
         $mensaje = 'Alumno Eliminado';
         $this->redireccionalumnos($alerta, $mensaje);
     }
-
-    
 }
